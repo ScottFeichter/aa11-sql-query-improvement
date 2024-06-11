3698acff1828be3773ccb81e3fc74ff79c590165
Scott Feichter <scottfeichter@gmail.com>
2024-06-10 19:54:11 -0700

    trying add -A

--- setup-commands.sh
+++ setup-commands.sh
@@ -18,6 +18,8 @@ sqlite3 practice.db ".read cat_toys.sql"
 
 # Check seed data
 
+sqlite3 ".mode box"
+
 echo "----- Tables Exist? -----"
 sqlite3 practice.db ".schema"
 
@@ -25,3 +27,14 @@ echo "----- Tables Have Data? -----"
 sqlite3 practice.db "SELECT 'cats_count', count(id) FROM cats;"
 sqlite3 practice.db "SELECT 'toys_count', count(id) FROM toys;"
 sqlite3 practice.db "SELECT 'cat_toys_count', count(id) FROM cat_toys;"
+
+
+echo "----- Step 0 Create Query -----"
+sqlite3 practice.db ".mode box SELECT * FROM cats JOIN cat_toys ON cat_toys.cat_id = cats.id WHERE toy_id = 5;"
+
+echo "----- Step 1 Analyze Query -----"
+sqlite3 practice.db "
+EXPLAIN QUERY PLAN
+SELECT * FROM cats
+JOIN cat_toys ON cat_toys.cat_id = cats.id
+WHERE toy_id = 5;"
